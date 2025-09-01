0f0c13b8b4b2874b6467bdc1a58e885514195bad
Yenzo0 <132693842+Yenzo0@users.noreply.github.com>
2025-08-31 23:33:15 -0400

    Add files via upload

---
+++ scratch.py
@@ -0,0 +1,30 @@
+#DATA FOR REFERENCE
+#Input	                      Output
+#5 multiplied by 6	The answer to your query is 30
+#8 divided by 3	The answer to your query is 2.66666667
+#8 divided by 3 without the remainder	The answer to your query is 2
+#8 modulo 3 (remainder only)	The answer to your query is 2
+#4200000 add 23090	The answer to your query is 4223090
+#4200000 subtract 7633	The answer to your query is 4192367
+#Name: Hugo Almeida
+#Homework: Variables Assignment
+
+Question1 = 5 * 6
+print("The answer to your query is", Question1)
+
+Question2 = round(8 / 3, 8)
+print("The answer to your query is", round(Question2, 8))
+
+Question3 = 8 // 3
+print("The answer to your query is", Question3)
+
+Question4 = 8 % 3
+print("The answer to your query is", Question4)
+
+Question5 = 4200000 + 23090
+print("The answer to your query is", Question5)
+
+Question6 = 4200000 - 7633
+print("The answer to your query is", Question6)
+
+print("Hugo Pires Almeida")
